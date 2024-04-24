9f50a31508e5584b02bcf6a2cbe0d0b56629debb
MichaelYang120 <michaelyangiscoding@gmail.com>
2024-04-24 07:40:54 -0500

    convert from spaces to tabs

--- src/index.ts
+++ src/index.ts
@@ -9,18 +9,21 @@ const uri = process.env.ATLAS_URI || "";
 
 const client = new MongoClient(uri);
 async function run() {
-  try {
-    await client.connect();
-    // database and collection code goes here
-    // insert code goes here
-    // display the results of your operation
-	console.log("Connected to the database");
-  } catch (e) {
+	try {
+		await client.connect();
+		// database and collection code goes here
+		// insert code goes here
+		// display the results of your operation
+		// Send a ping to confirm a successful connection
+		await client.db("admin").command({ ping: 1 });
+		console.log("Pinged your deployment. You successfully connected to MongoDB!");
+		console.log("Connected to the database");
+	} catch (e) {
 	  console.log("Error connecting to the database: ", e);
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
+	} finally {
+		// Ensures that the client will close when you finish/error
+		await client.close();
+	}
 }
 
 run().catch(console.dir);
